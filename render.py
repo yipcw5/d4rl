0d6c0db37dadcdeaa0f48b5991357e39e2c67ef5
yipcw5 <yipcw9@gmail.com>
2022-02-02 13:18:25 +0000

    Working installation locally

---
+++ render.py
@@ -0,0 +1,10 @@
+import gym
+import d4rl 
+
+
+env = gym.make('door-v0')
+env.mujoco_render_frames = True
+obs = env.reset()
+for t in range(500):
+    _ = env.step(env.action_space.sample())
+    env.mj_render()
--- setup.py
+++ setup.py
@@ -8,13 +8,10 @@ setup(
     version='1.1',
     install_requires=['gym',
                       'numpy',
-                      'mujoco_py',
                       'pybullet',
                       'h5py',
                       'termcolor',  # adept_envs dependency
                       'click',  # adept_envs dependency
-                      'dm_control' if 'macOS' in platform() else
-                      'dm_control @ git+git://github.com/deepmind/dm_control@master#egg=dm_control',
                       'mjrl @ git+git://github.com/aravindr93/mjrl@master#egg=mjrl'],
     packages=find_packages(),
     package_data={'d4rl': ['locomotion/assets/*',
