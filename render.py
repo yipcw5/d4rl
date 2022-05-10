6ba765e45e2965f9c230854173710aa10b22c698
yipcw5 <yipcw9@gmail.com>
2022-05-10 16:24:04 +0100

    test background of body - unsuccessful in openpose

--- render.py
+++ render.py
@@ -10,15 +10,19 @@ env = gym.make('door-v0')
 env.mujoco_render_frames = True
 obs = env.reset()
 
-for t in range(300):
+
+
+for t in range(125):
     qp = np.array([0.0] * 30)
-    qp[2] = 1.57
+    qp[2] = 1.2 # azimuth
     #qp[1] = -1.57 # vertical
     qp[3] = 1.57 # faceup
-
-    qp[7] += radians(136-117) * s/50
-    qp[8] += radians(171-95) * s/50
-    qp[9] += radians(140-65) * t/50
+    iters = 100
+    if t>iters: s=iters
+    else: s=t
+    qp[7] += radians(136-117) * s/iters
+    qp[8] += radians(171-95) * s/iters
+    qp[9] += radians(140-65) * s/iters
     _ = env.step(qp)
     #print('www',env.action_space.sample())
     env.mj_render()
