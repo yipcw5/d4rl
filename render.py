f5082a4b26bfb6b10ea55409af6a0e5350ba06f1
yipcw5 <yipcw9@gmail.com>
2022-05-10 14:45:36 +0100

    camera and background changes, scipy for matlab

--- render.py
+++ render.py
@@ -4,21 +4,21 @@ from mujoco_env import MujocoEnv
 
 import numpy as np
 from math import radians
+from scipy.io import loadmat
 
 env = gym.make('door-v0')
 env.mujoco_render_frames = True
 obs = env.reset()
 
-for t in range(500):
+for t in range(300):
     qp = np.array([0.0] * 30)
-    #qp[2] = 1.57
-    qp[1] = -1.57 # vertical
-    qp[3] = 0.7 # faceup
-    if t < 300: s = t
-    else: s = 300
-    qp[7] += radians(136-117) * s/300
-    qp[8] += radians(171-95) * s/300
-    qp[9] += radians(140-65) * s/300
+    qp[2] = 1.57
+    #qp[1] = -1.57 # vertical
+    qp[3] = 1.57 # faceup
+
+    qp[7] += radians(136-117) * s/50
+    qp[8] += radians(171-95) * s/50
+    qp[9] += radians(140-65) * t/50
     _ = env.step(qp)
     #print('www',env.action_space.sample())
     env.mj_render()
